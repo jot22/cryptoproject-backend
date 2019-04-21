c374ef036ce805244a62103589e613be92738f08
Jerry Lanning <lanning.j@husky.neu.edu>
2019-04-21 17:49:48 -0400

    Debugging

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => res.send('Hello World'));
 app.use('/api', apiRoutes)
 // Launch app to listen to specified port
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
